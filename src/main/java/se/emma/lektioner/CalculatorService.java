a7a082db0f05be3b2f706b13975bca3aab5d4100
emmaalmer <emmm1019@gmail.com>
2025-04-28 12:37:54 +0200

    lektionsuppgifter 1

--- src/main/java/se/emma/lektioner/CalculatorService.java
+++ src/main/java/se/emma/lektioner/CalculatorService.java
@@ -1,4 +1,29 @@
 package se.emma.lektioner;
 
 public class CalculatorService {
+
+    public double addition(double a, double b){
+
+        double c = Math.round( (a + b)*10.0)/10.0;
+        return c;
+    }
+    public int subtract(int a, int b){
+
+        int c = a - b;
+        return c;
+    }
+    public int multiply(int a, int b){
+
+        int c = a * b;
+        return c;
+    }
+    public int divide(int a, int b){
+
+        if ( b == 0){
+            return -1;
+        }
+
+        int c = a / b;
+        return c;
+    }
 }
--- src/test/java/se/emma/lektioner/CalculatorServiceTest.java
+++ src/test/java/se/emma/lektioner/CalculatorServiceTest.java
@@ -1,8 +1,35 @@
 package se.emma.lektioner;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorServiceTest {
 
-    public static void main(String[] args) {
+    CalculatorService cs = new CalculatorService();
 
+    @DisplayName("add")
+    @Test
+    void additionTest(){
+
+        assertEquals( 0.3, cs.addition(0.1, 0.2));
+
+    }
+    @DisplayName("sub")
+    @Test
+    void subtractTest(){
+        assertEquals( 0, cs.subtract(2, 2));
+    }
+    @DisplayName("multi")
+    @Test
+    void multiplyTest(){
+        assertEquals( 4, cs.multiply(2, 2));
+
+    }
+    @DisplayName("div")
+    @Test
+    void divideTest(){
+        assertEquals( -1, cs.divide(2, 0));
     }
 }
